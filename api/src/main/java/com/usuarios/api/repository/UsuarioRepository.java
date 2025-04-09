3f11ae717ffee6c97258f8211597293076b7fed8
@_Ingoat <igolwb@gmail.com>
2025-04-09 00:23:47 -0300

    Delete api directory

--- api/src/main/java/com/usuarios/api/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.usuarios.api.repository;
-
-import com.usuarios.api.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
